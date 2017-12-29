c0be51261f40394d68f876beec4b632f9ae7cec2
unknown <jovanbelic234@gmail.com>
2017-12-29 18:12:49 +0100

    finished design page

--- views/app.js
+++ views/app.js
@@ -1,1 +1,3 @@
 const home = document.getElementById('home').addEventListener('click',function(){document.location = "index.html";})
+
+const learn_more = document.getElementById('learn_more').addEventListener('click',function(){document.location = "design.html";})
